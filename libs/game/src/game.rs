c9b180321b136e3b7891fcd68469ca4385696406
Ryan1729 <Ryan1729@gmail.com>
2025-06-03 01:06:00 -0600

    add a fixed map

--- libs/game/src/game.rs
+++ libs/game/src/game.rs
@@ -1,4 +1,5 @@
 use models::{TileKind};
+use maps::houses;
 use platform_types::{command, unscaled};
 use xs::{Xs, Seed};
 
@@ -25,8 +26,23 @@ pub mod xy {
         }
     }
 
-    impl core::ops::AddAssign for X {
-        fn add_assign(&mut self, other: Self) {
+    #[derive(Clone, Copy, Default)]
+    pub struct W(Inner);
+
+    impl W {
+        pub const ONE: Self = Self(1);
+
+        pub fn usize(self) -> usize {
+            self.0.into()
+        }
+    }
+
+    pub fn w(w: Inner) -> W {
+        W(if w > MAX_W_INNER { MAX_W_INNER } else { w })
+    }
+
+    impl core::ops::AddAssign<W> for X {
+        fn add_assign(&mut self, other: W) {
             self.0 = self.0.saturating_add(other.0);
             if self.0 > MAX_W_INNER {
                 self.0 = MAX_W_INNER;
@@ -34,8 +50,8 @@ pub mod xy {
         }
     }
 
-    impl core::ops::SubAssign for X {
-        fn sub_assign(&mut self, other: Self) {
+    impl core::ops::SubAssign<W> for X {
+        fn sub_assign(&mut self, other: W) {
             self.0 = self.0.saturating_sub(other.0);
         }
     }
@@ -58,8 +74,23 @@ pub mod xy {
         }
     }
 
-    impl core::ops::AddAssign for Y {
-        fn add_assign(&mut self, other: Self) {
+    #[derive(Clone, Copy, Default)]
+    pub struct H(Inner);
+
+    impl H {
+        pub const ONE: Self = Self(1);
+
+        pub fn usize(self) -> usize {
+            self.0.into()
+        }
+    }
+
+    pub fn h(h: Inner) -> H {
+        H(if h > MAX_H_INNER { MAX_H_INNER } else { h })
+    }
+
+    impl core::ops::AddAssign<H> for Y {
+        fn add_assign(&mut self, other: H) {
             self.0 = self.0.saturating_add(other.0);
             if self.0 > MAX_H_INNER {
                 self.0 = MAX_H_INNER;
@@ -67,13 +98,13 @@ pub mod xy {
         }
     }
 
-    impl core::ops::SubAssign for Y {
-        fn sub_assign(&mut self, other: Self) {
+    impl core::ops::SubAssign<H> for Y {
+        fn sub_assign(&mut self, other: H) {
             self.0 = self.0.saturating_sub(other.0);
         }
     }
 }
-pub use xy::{X, Y};
+pub use xy::{X, Y, W, H};
 
 #[derive(Clone, Default)]
 pub struct Entity {
@@ -93,7 +124,7 @@ pub struct Tile {
 pub struct State {
     pub rng: Xs,
     pub player: Entity,
-    pub tiles: Vec<Tile>,
+    pub scratch_tile: Tile,
 }
 
 #[derive(Clone, Copy)]
@@ -107,71 +138,25 @@ pub enum Dir {
 fn move_entity(entity: &mut Entity, dir: Dir) {
     use Dir::*;
     match dir {
-        Up => { entity.y -= Y::ONE; },
-        Down => { entity.y += Y::ONE; },
-        Left => { entity.x -= X::ONE; },
-        Right => { entity.x += X::ONE; },
+        Up => { entity.y -= H::ONE; },
+        Down => { entity.y += H::ONE; },
+        Left => { entity.x -= W::ONE; },
+        Right => { entity.x += W::ONE; },
     }
 }
 
 impl State {
     pub fn new(seed: Seed) -> State {
+        use maps::{houses};
+
         let rng = xs::from_seed(seed);
 
         let mut player = Entity::default();
         player.kind = 9;
         player.x = xy::x(2);
 
-        // TODO define a whole fixed map
-        //    Maybe define macros or something to make it nicer?
-        //    Actually probably take an array of kinds or something, then add the coords.
-        //        Or maybe make it a dense map of tiles?
-        let mut tiles = vec![
-            Tile {
-                kind: 0,
-                x: xy::x(0),
-                y: xy::y(0),
-            },
-            Tile {
-                kind: 1,
-                x: xy::x(1),
-                y: xy::y(0),
-            },
-            Tile {
-                kind: 2,
-                x: xy::x(2),
-                y: xy::y(0),
-            },
-            Tile {
-                kind: 3,
-                x: xy::x(3),
-                y: xy::y(0),
-            },
-            Tile {
-                kind: 14,
-                x: xy::x(0),
-                y: xy::y(1),
-            },
-            Tile {
-                kind: 15,
-                x: xy::x(1),
-                y: xy::y(1),
-            },
-            Tile {
-                kind: 15, // On purpose!
-                x: xy::x(2),
-                y: xy::y(1),
-            },
-            Tile {
-                kind: 17,
-                x: xy::x(3),
-                y: xy::y(1),
-            },
-        ];
-
         State {
             rng,
-            tiles,
             player,
             .. <_>::default()
         }
@@ -181,7 +166,75 @@ impl State {
         move_entity(&mut self.player, dir);
     }
 
-    pub fn current_tiles(&self) -> impl Iterator<Item = &Tile> {
-        self.tiles.iter()
+    pub fn current_tiles(&self) -> impl Iterator<Item = Tile> {
+        let top_left = 0; // TODO set based on player position
+
+        CameraIter {
+            tiles: &houses::TILES,
+            tiles_width: houses::WIDTH as _,
+            top_left, 
+            output_width: xy::w(32),
+            output_height: xy::h(24),
+            tiles_index: top_left,
+            tile: Tile::default(),
+        }
+    }
+}
+
+struct CameraIter<'tiles> {
+    tile: Tile,
+    tiles: &'tiles [TileKind],
+    tiles_width: usize,
+    tiles_index: usize,
+    top_left: usize,
+    output_width: xy::W,
+    output_height: xy::H,
+}
+
+impl Iterator for CameraIter<'_> {
+    type Item = Tile;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(tile_kind) = self.tiles.get(self.tiles_index) {
+            self.tile.kind = *tile_kind;
+
+            let output = self.tile.clone();
+
+            self.tiles_index += 1;
+            self.tile.x += xy::w(1);
+
+            // The x position in the entire field of tiles
+            let tile_x = self.tiles_index % self.tiles_width;
+
+            // The x position of the left edge of the view
+            let left_x = self.top_left % self.tiles_width;
+            let right_x = left_x + self.output_width.usize();
+
+            if tile_x > right_x {
+                self.tiles_index = 
+                    // Move back to the start of the row
+                    self.tiles_index - (self.output_width.usize() + 1)
+                    // Move down a row
+                    + self.tiles_width as usize;
+                self.tile.x = xy::x(0);
+                self.tile.y += xy::h(1);
+            }
+
+            // The y position in the entire field of tiles
+            let tile_y = self.tiles_index / self.tiles_width;
+
+            // The y position of the top edge of the view
+            let top_y = self.top_left / self.tiles_width;
+            let bottom_y = top_y + self.output_height.usize();
+
+            if tile_y > bottom_y {
+                // Ensure we hit return None next time
+                self.tiles_index = self.tiles.len();
+            }
+
+            return Some(output)
+        }
+
+        None
     }
 }
